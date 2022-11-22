79eb0b75b642aff8451b98dddc00702747bb12ec
jfoyarzo <foyarzo621@gmail.com>
2022-11-22 13:48:08 -0300

    Add react-bootstrap dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Main APP</h1>
+      <Button className="ms-4" variant="warning">Button</Button>
     </div>
   );
 }
