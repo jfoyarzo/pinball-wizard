1ff6e3c5df59f3c9d640aa988b3a230df64598ff
jfoyarzo <foyarzo621@gmail.com>
2022-11-22 20:06:33 -0300

    Add action dispatch to fetch to total machine count

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,18 @@
-import { React } from 'react';
+import { React, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getMachines } from '../redux/machines/machinesSlice';
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMachines());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
 
 export default Home;
