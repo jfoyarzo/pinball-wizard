5b27ea06d8c230e2e1ceb0b3100465cd4481cf68
jfoyarzo <foyarzo621@gmail.com>
2022-11-22 18:09:54 -0300

    Add conditional link render to the back arrow of the navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import { MdArrowBackIosNew, MdOutlineMic, MdSettings } from 'react-icons/md';
@@ -7,6 +7,15 @@ import { MdArrowBackIosNew, MdOutlineMic, MdSettings } from 'react-icons/md';
 const MainNav = () => {
   const [title, setTitle] = useState('Most Machines');
   const location = useLocation();
+  let backButton = <MdArrowBackIosNew />;
+
+  if (location.pathname !== '/') {
+    backButton = (
+      <NavLink to="/">
+        <MdArrowBackIosNew />
+      </NavLink>
+    );
+  }
 
   useEffect(() => {
     if (location.pathname === '/') {
@@ -19,7 +28,7 @@ const MainNav = () => {
   return (
     <Navbar style={{ height: '4vh', backgroundColor: 'blue' }}>
       <Container className="align-middle justify-content-between">
-        <MdArrowBackIosNew />
+        {backButton}
         <p>{title}</p>
         <div>
           <MdOutlineMic />
