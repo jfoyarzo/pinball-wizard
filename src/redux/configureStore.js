d73e0be8f3113077890ec867a12c360abbc6e7f0
jfoyarzo <foyarzo621@gmail.com>
2022-11-22 23:06:58 -0300

    Add topCities.js state slice file

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import machinesReducer from './machines/machinesSlice';
+import topCitiesReducer from './topCities/topCities';
 
 const reducer = {
   totalMachines: machinesReducer,
+  topCities: topCitiesReducer,
 };
 
 const store = configureStore({ reducer });
