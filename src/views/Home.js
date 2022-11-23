7d38e028fb977c2bbaba09d58a079df1c207925b
jfoyarzo <foyarzo621@gmail.com>
2022-11-22 23:10:24 -0300

    Add top cities dynamic list with machine counts

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,10 @@
 import { React, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { getMachines } from '../redux/machines/machinesSlice';
 import usa from '../img/usa_map.png';
+import pinballIcon from '../img/pinball_icon.png';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -11,9 +13,11 @@ const Home = () => {
   }, [dispatch]);
 
   const totalMachines = useSelector((state) => state.totalMachines);
+  const top = useSelector((state) => state.topCities);
+
   return (
     <div className="home-wrapper">
-      <Row xs="2" style={{ height: '30vh', padding: '1em', backgroundColor: 'blue' }}>
+      <Row xs="2" style={{ height: '25vh', padding: '1em', backgroundColor: 'blue' }}>
         <Col style={{
           backgroundImage: `url(${usa})`, backgroundColor: 'blue', backgroundBlendMode: 'multiply', backgroundSize: '20vh', backgroundRepeat: 'no-repeat', backgroundPosition: 'center',
         }}
@@ -27,6 +31,34 @@ const Home = () => {
           </p>
         </Col>
       </Row>
+      <Container className="border border-primary">
+        <p>Top Cities</p>
+      </Container>
+      <div className="top-container">
+        <Row>
+          {top.map((region) => (
+            <Col
+              xs="6"
+              key={region.city}
+              className="p-4 border"
+              style={{
+                backgroundImage: `url(${pinballIcon})`, backgroundSize: '13vh', backgroundRepeat: 'no-repeat', backgroundPosition: 'top', backgroundColor: 'blue', backgroundBlendMode: 'multiply',
+              }}
+            >
+              <Link to={`/${region.city}`}>
+                <div className="d-flex flex-column justify-content-end" style={{ height: '15vh' }}>
+                  <h3>{region.city}</h3>
+                  <p>
+                    Machine Count:
+                    {' '}
+                    {region.machineCount}
+                  </p>
+                </div>
+              </Link>
+            </Col>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 };
