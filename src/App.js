d33a419b33885f7c07f816547708a20fc7d46853
jfoyarzo <foyarzo621@gmail.com>
2022-11-22 23:12:02 -0300

    Update App.js file with useEffect to fetch the top cities when the app loads and update routes for each region

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import './App.css';
 import MainNav from './Components/Navbar';
 import Home from './views/Home';
 import UnderConstruction from './views/Under_Construction';
+import { getCities } from './redux/topCities/topCities';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCities());
+  }, [dispatch]);
+
+  const top = useSelector((state) => state.topCities);
   return (
     <Router>
       <MainNav />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="*" element={<UnderConstruction />} />
+        {top.map((region) => <Route path={`/${region.city}`} element={<UnderConstruction />} key={region.city} />)}
       </Routes>
     </Router>
   );
