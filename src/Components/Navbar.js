188256335d6905cf66e19ac7da25159f975e5b71
jfoyarzo <foyarzo621@gmail.com>
2022-11-24 16:50:23 -0300

    Add style to Navbar component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,12 +2,18 @@ import { React, useState, useEffect } from 'react';
 import { useLocation, NavLink } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import { IconContext } from 'react-icons';
 import { MdArrowBackIosNew, MdOutlineMic, MdSettings } from 'react-icons/md';
 
 const MainNav = () => {
   const [title, setTitle] = useState('Most Machines');
   const location = useLocation();
-  let backButton = <MdArrowBackIosNew />;
+  let backButton = (
+    <div className="d-flex align-items-center">
+      <MdArrowBackIosNew />
+      <span className="fw-bold" style={{ fontSize: '1.4em' }}>2015</span>
+    </div>
+  );
 
   if (location.pathname !== '/') {
     backButton = (
@@ -19,23 +25,25 @@ const MainNav = () => {
 
   useEffect(() => {
     if (location.pathname === '/') {
-      setTitle('Most Machines');
+      setTitle('most machines');
     } else {
       setTitle('City view');
     }
   }, [location, setTitle]);
 
   return (
-    <Navbar style={{ height: '4vh', backgroundColor: 'blue' }}>
-      <Container className="align-middle justify-content-between">
-        {backButton}
-        <p>{title}</p>
-        <div>
-          <MdOutlineMic />
-          <MdSettings />
-        </div>
-      </Container>
-    </Navbar>
+    <IconContext.Provider value={{ size: '1.6em' }}>
+      <Navbar style={{ height: '5vh' }}>
+        <Container className="align-middle justify-content-between">
+          {backButton}
+          <p className="fw-light" style={{ fontSize: '1.2em' }}>{title}</p>
+          <div>
+            <MdOutlineMic className="me-3" />
+            <MdSettings />
+          </div>
+        </Container>
+      </Navbar>
+    </IconContext.Provider>
   );
 };
 
